336b8276e6d835c8828213dfc1e1fd8ac14588d0
Jeremy Malara <jeremy@malara.net>
2018-11-04 22:10:14 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
+
 cd /home/tc/beepbeep
-git fetch --all
-git reset --hard origin/master
-git pull origin master
+
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    git fetch --all
+    git reset --hard origin/master
+    git pull origin master
+fi
+
 sudo python beep.py &
