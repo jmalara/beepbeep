e9b2c6a5f70e4716743a816415394e717db69fe2
Jeremy Malara <jeremy@malara.net>
2018-11-04 22:11:21 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 
 cd /home/tc/beepbeep
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+echo -e "GET http://gasafsfasfle.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     git fetch --all
