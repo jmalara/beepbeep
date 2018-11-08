aca2b8313814c303ba1c6f409577ef54f8b50c9c
Jeremy Malara <jeremy@malara.net>
2018-11-08 00:08:07 -0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -17,21 +17,23 @@ GPIO.setup(horn_input, GPIO.IN)
 GPIO.setup(relay_output, GPIO.OUT)
 
 # Handle horn press event
-def hornEventHandler_rising (pin):
-    # First beep
-    print("beep on")
-    GPIO.output(relay_output,True)
-    time.sleep(float(beep_duration))	
-    # Delay between beeps
-    GPIO.output(relay_output,False)
-    print("beep off")
-    time.sleep(float(beep_delay))
-    # Second beep
-    print("beep on")
-    GPIO.output(relay_output,True)
-    time.sleep(float(beep_duration))
-    GPIO.output(relay_output,False)
-    print("beep off")
+def hornEventHandler_rising (horn_input):
+    time.sleep(.01)
+    if GPIO.input(horn_input):
+        # First beep
+        print("beep on")
+        GPIO.output(relay_output,True)
+        time.sleep(float(beep_duration))	
+        # Delay between beeps
+        GPIO.output(relay_output,False)
+        print("beep off")
+        time.sleep(float(beep_delay))
+        # Second beep
+        print("beep on")
+        GPIO.output(relay_output,True)
+        time.sleep(float(beep_duration))
+        GPIO.output(relay_output,False)
+        print("beep off")
 	
 GPIO.add_event_detect(horn_input, GPIO.RISING, callback=hornEventHandler_rising, bouncetime=300) 
  
