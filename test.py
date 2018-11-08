4968aad625728283a97eedbda6b282efdae54d5f
Jeremy Malara <jeremy@malara.net>
2018-11-08 00:40:37 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ GPIO.setup(2, GPIO.IN)    # set GPIO 25 as input
 try:  
     while True:            # this will carry on until you hit CTRL+C  
         if GPIO.input(2): # if port 25 == 1  
-            print "Port 2 is 1/GPIO.HIGH/True - button pressed"  
+            print "Port 2 is 1/GPIO.HIGH/True - Horn Button Off"  
         else:  
-            print "Port 2 is 0/GPIO.LOW/False - button not pressed"  
+            print "Port 2 is 0/GPIO.LOW/False - Horn Button ON"  
         sleep(0.1)         # wait 0.1 seconds  
   
 except KeyboardInterrupt:  
