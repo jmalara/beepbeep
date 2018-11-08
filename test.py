2e55ae356db7b4e197905184091429897d13eadf
Jeremy Malara <jeremy@malara.net>
2018-11-08 00:33:18 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO  
 from time import sleep     # this lets us have a time delay (see line 12)  
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(2, GPIO.IN)    # set GPIO 25 as input  
+GPIO.setup(4, GPIO.IN)    # set GPIO 25 as input  
   
 try:  
     while True:            # this will carry on until you hit CTRL+C  
-        if GPIO.input(2): # if port 25 == 1  
-            print "Port 2 is 1/GPIO.HIGH/True - button pressed"  
+        if GPIO.input(4): # if port 25 == 1  
+            print "Port 4 is 1/GPIO.HIGH/True - button pressed"  
         else:  
-            print "Port 2 is 0/GPIO.LOW/False - button not pressed"  
+            print "Port 4 is 0/GPIO.LOW/False - button not pressed"  
         sleep(0.1)         # wait 0.1 seconds  
   
 except KeyboardInterrupt:  
