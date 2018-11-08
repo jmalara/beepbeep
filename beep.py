9d13f7df4219e204fda9b89fd299f87994e09240
Jeremy Malara <jeremy@malara.net>
2018-11-08 00:49:58 -0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -17,7 +17,9 @@ GPIO.setup(horn_input, GPIO.IN)
 GPIO.setup(relay_output, GPIO.OUT)
 
 time.sleep(.1)
-if GPIO.input(horn_input): # if port 25 == 1  
+if GPIO.input(horn_input): # if port 25 == 1
+    print("off")
+else
     print "Port 2 is 1/GPIO.HIGH/True - Horn Button Off"  
     # First beep
     print("beep on")
